2ec3e0a2e48fa04e265ba0d9ada3d977c4ddc92f
SenninSage333 <58287037+SenninSage333@users.noreply.github.com>
2020-04-05 20:31:01 +0200

    wywolania funkcji

---
+++ zadania/ZadWywolanieFunkcji.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+void funA(){
+    cout << "Funkcja A" << endl;
+}
+
+void funB(){
+    cout << "Funkcja B" << endl;
+}
+
+void error(){
+    cout << "There is no such function" << endl;
+}
+
+int main(int argc, char* argv[]){
+
+    //1 sposob - switch
+    for(int i = 1; i < argc; i++){
+        switch(*argv[i]){
+            case '1': funA(); break;
+            case '2': funB(); break;
+            default: error(); break;
+        }
+    }
+
+    //2 sposob - operator ternarny
+    char c;
+    cout << "Wybierz funkcje: 1 - funA(), 2 - funB()" << endl;
+    cin >> c;
+    (c == '1') ? funA() : ((c == '2') ? funB() : error());
+    return 0;
+}
